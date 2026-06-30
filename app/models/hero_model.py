d81301dd7c35210187d87130c71e8d9359a2b747
successdevop <successraphael28@gmail.com>
2026-06-30 20:03:21 +0100

    testing the sqlmodel exec method

--- app/models/hero_model.py
+++ app/models/hero_model.py
@@ -5,9 +5,9 @@ from sqlmodel import SQLModel, Field, create_engine, Session, select
 
 class Hero(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    name: str = Field(index=True)
     secret_name: str
-    age: Optional[int] = None
+    age: Optional[int] = Field(default=None, index=True)
 
 
 sqlite_file_name = "../database/database.db"
@@ -16,25 +16,34 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 engine = create_engine(sqlite_url, echo=True)
 
 def create_heroes():
-    hero1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-    hero2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-    hero3 = Hero(name="Rusty-man", secret_name="Tommy Sharp", age=48)
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
-        session.add(hero1)
-        session.add(hero2)
-        session.add(hero3)
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
+        statement = select(Hero).where(Hero.name == "Deadpond")
         result = session.exec(statement)
         heroes = result.all()
+        
         print(result)
         print(heroes)
-        
+
         if result is not None:
             for hero in result:
                 print(hero)
