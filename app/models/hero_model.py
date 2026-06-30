c21ce870e1b3582f9d39f44b05f7ac9a5fd5190e
successdevop <successraphael28@gmail.com>
2026-06-30 10:55:28 +0100

    testing with sqlmodel database and sqlite

--- app/models/hero_model.py
+++ app/models/hero_model.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 
 
 class Hero(SQLModel, table=True):
@@ -10,8 +10,44 @@ class Hero(SQLModel, table=True):
     age: Optional[int] = None
 
 
-sqlite_file_name = "../database/my_database.db"
-sqlite_url = f"sqlite:///my_database.db"
+sqlite_file_name = "../database/database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
-SQLModel.metadata.create_all(engine)
+
+def create_heroes():
+    hero1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero3 = Hero(name="Rusty-man", secret_name="Tommy Sharp", age=48)
+
+    with Session(engine) as session:
+        session.add(hero1)
+        session.add(hero2)
+        session.add(hero3)
+
+        session.commit()
+
+def select_heroes():
+    with Session(engine) as session:
+        statement = select(Hero)
+        result = session.exec(statement)
+        heroes = result.all()
+        print(result)
+        print(heroes)
+        
+        if result is not None:
+            for hero in result:
+                print(hero)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def main():
+    create_db_and_tables()
+    create_heroes()
+    select_heroes()
+
+if __name__ == "__main__":
+    main()
