425d91ee39a9ae4e73fa8d40c64340d6b1abefc9
successdevop <successraphael28@gmail.com>
2026-07-01 20:36:58 +0100

    tested the sqlmodel relationship among models to inspect it's functionality

--- app/models/hero_model.py
+++ app/models/hero_model.py
@@ -37,29 +37,22 @@ def create_heroes():
         team_preventers = Team(name="Preventers", headquarters="Sharp Towers")
         team_z_force = Team(name="Z-Force", headquarters="Sister Margaret's Bar")
 
-        hero_deadpond = Hero(name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id)
-        hero_rusty_man = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48, team_id=team_preventers.id)
+        hero_deadpond = Hero(name="Deadpond", secret_name="Dive Wilson", team=team_preventers)
+        hero_rusty_man = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48, team=team_z_force)
         hero_spider_boy = Hero(name="Spider Boy", secret_name="Pedro Parqueador")
 
-        team = hero_deadpond.team.name
-        print(team)
-
-        # session.add(team_preventers)
-        # session.add(team_z_force)
-        # session.commit()
-
-        # session.add(hero_deadpond)
-        # session.add(hero_rusty_man)
-        # session.add(hero_spider_boy)
-        # session.commit()
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
         #
-        # session.refresh(hero_deadpond)
-        # session.refresh(hero_rusty_man)
-        # session.refresh(hero_spider_boy)
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
         #
-        # print("Created hero: ", hero_deadpond)
-        # print("Created hero: ", hero_rusty_man)
-        # print("Created hero: ", hero_spider_boy)
+        print("Created hero: ", hero_deadpond)
+        print("Created hero: ", hero_rusty_man)
+        print("Created hero: ", hero_spider_boy)
 
 
 def select_one():
