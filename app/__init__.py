b10a17ff3891ff897e4c3d47c0a19782b4410e0b
successdevop <successraphael28@gmail.com>
2026-07-08 15:45:20 +0100

    body request parameter tested in fastapi

--- app/__init__.py
+++ app/__init__.py
@@ -168,6 +168,7 @@
 #
 # print(say_hello())
 from enum import Enum
+from typing import Any
 
 # class ModelName(str, Enum):
 #     AlexNet = "alexnet"
@@ -191,37 +192,69 @@ from enum import Enum
 #     return {"file_path": file_path}
 
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
+#
+#
+# app = FastAPI()
+#
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+#
+#
+# @app.get("/")
+# def read_items(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+#
+#
+# @app.get("/item/{item_id}")
+# def read_item(item_id: str, q: str | None = None, short: bool = False)-> dict[str, int | str | None]:
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#
+#     if short:
+#         item.update({"description":"This is an amazing item that has a long description"})
+#
+#     return item
+#
+#
+# @app.get("/users/{user_id}/item/{item_id}")
+# def read_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False) -> dict[str, int | str | None]:
+#     item = {"item_id": item_id, "owner_id": user_id}
+#     if q:
+#         item.update({"q": q})
+#     if short:
+#         item.update({"description":"This is an amazing item that has a long description"})
+#
+#     return item
 
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
-@app.get("/")
-def read_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+@app.post("/items", response_model=dict[str, Any], status_code=200)
+def create_item(item: Item) -> dict[str, Any]:
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.tax + item.price
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
-@app.get("/item/{item_id}")
-def read_item(item_id: str, q: str | None = None, short: bool = False)-> dict[str, int | str | None]:
-    item = {"item_id": item_id}
+@app.patch("/items/{item_id}")
+def update_item(item_id: int, item: Item, q: str | None = None):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.tax + item.price
+        item_dict.update({"price_with_tax":price_with_tax})
     if q:
-        item.update({"q": q})
-
-    if short:
-        item.update({"description":"This is an amazing item that has a long description"})
-
-    return item
-
-
-@app.get("/users/{user_id}/item/{item_id}")
-def read_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False) -> dict[str, int | str | None]:
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if short:
-        item.update({"description":"This is an amazing item that has a long description"})
-
-    return item
+        item_dict.update({"q": q})
+    return {"item_id": item_id, **item_dict}
+    # return {"item_id": item_id, **item.model_dump()}
