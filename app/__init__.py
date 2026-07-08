555c26a6760e0974e3906d0f430c2cf1f449a79e
successdevop <successraphael28@gmail.com>
2026-07-08 14:19:00 +0100

    query parameter checked and tested in sqlmodel

--- app/__init__.py
+++ app/__init__.py
@@ -169,29 +169,59 @@
 # print(say_hello())
 from enum import Enum
 
+# class ModelName(str, Enum):
+#     AlexNet = "alexnet"
+#     ResNet = "resnet"
+#     LeNet = "lenet"
+#
+#
+# @app.get("/models/{model_name}")
+# async def root(model_nam: ModelName):
+#     if model_nam is model_nam.AlexNet:
+#         return {"model_name": model_nam, "message":"Deep Learning FTW!"}
+#
+#     if model_nam.value == "lenet":
+#         return {"model_name": model_nam, "message":"LeCNN all the images"}
+#
+#     return {"model_name":model_nam, "message":"Have some residuals"}
+#
+#
+# @app.get("/files/{file_path:path}")
+# async def read_file(file_path: str):
+#     return {"file_path": file_path}
+
+
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
-
-class ModelName(str, Enum):
-    AlexNet = "alexnet"
-    ResNet = "resnet"
-    LeNet = "lenet"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/models/{model_name}")
-async def root(model_nam: ModelName):
-    if model_nam is model_nam.AlexNet:
-        return {"model_name": model_nam, "message":"Deep Learning FTW!"}
-
-    if model_nam.value == "lenet":
-        return {"model_name": model_nam, "message":"LeCNN all the images"}
-
-    return {"model_name":model_nam, "message":"Have some residuals"}
+@app.get("/")
+def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+@app.get("/item/{item_id}")
+def read_item(item_id: str, q: str | None = None, short: bool = False)-> dict[str, int | str | None]:
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+
+    if short:
+        item.update({"description":"This is an amazing item that has a long description"})
+
+    return item
+
+
+@app.get("/users/{user_id}/item/{item_id}")
+def read_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False) -> dict[str, int | str | None]:
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if short:
+        item.update({"description":"This is an amazing item that has a long description"})
+
+    return item
