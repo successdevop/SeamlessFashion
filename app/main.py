96aebf9e74b97a685a90342353086615ff3f9894
successdevop <successraphael28@gmail.com>
2026-07-09 18:33:04 +0100

    tested path parameter and numeric validation with fastapi

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
-from typing import Annotated
+from typing import Annotated, Literal
 
 from fastapi import FastAPI, Path
 from fastapi.params import Query
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+    model_config = {"extra":"forbid"}
+
+
+@app.get("/item")
+def get_data(filter_query: Annotated[FilterParams, Query()]):
+    print(filter_query.order_by)
+    return filter_query
 
 
 @app.get("/items/{item_id}")
@@ -16,3 +31,5 @@ def read_items(*, item_id: Annotated[int, Path(title="The ID of the item to get"
     if size:
         result.update({"size":size})
     return result
+
+
