6e18d5f8ffb2fc9023d1b275d7713920448ea103
successdevop <successraphael28@gmail.com>
2026-07-10 09:29:15 +0100

    tested openApi and schema data conversion, validation and documentation in fastapi

--- app/main.py
+++ app/main.py
@@ -21,12 +21,27 @@ class Image(BaseModel):
 
 
 class Item(BaseModel):
+    name: str = Field(examples=["Foo"])
+    description: str | None = Field(default=None, examples=["A very nice item"])
+    price: float= Field(examples=[35.4])
+    tax: float | None = Field(default=None, examples=[3.2])
+    tags: set[str] = Field(examples=[["foo", "doe"]])
+    image: list[Image] | None = Field(examples=[{"name": "Google", "url":"https://www.google.com"}])
+
+    # model_config = {
+    #     "json_schema_extra":{
+    #         "examples":[
+    #             {"name": "Foo", "description": "A very nice item", "price": 35.4, "tax":3.2}
+    #         ]
+    #     }
+    # }
+
+
+class Item2(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-    tags: set[str] = set()
-    image: list[Image] | None = None
 
 
 class User(BaseModel):
@@ -41,23 +56,45 @@ class Offer(BaseModel):
     items: list[Item]
 
 
-@app.post("/index-weights")
-def create_index_weights(weights: dict[int, float]):
-    return weights
-
-
-@app.post("/image/multiple")
-def create_images(images: list[Image]):
-    return images
-
-
-@app.post("/offers")
-def create_offers(offer: Offer):
-    return offer
-
-
 @app.put("/items/{item_id}")
-def read_data(item_id: int, item: Item):
-    result = {"item_id": item_id, "item_data": item}
-    return result
+def update(item_id: int,
+           item: Annotated[Item2, Body(
+               examples=[
+                   {
+                       "name":"Foo",
+                       "description":"A very nice item",
+                       "price": 35.4,
+                       "tax": 3.2
+                   },
+                   {
+                       "name": "Bar",
+                       "price": "35.4",
+                   },
+                   {
+                       "name": "Baz",
+                       "price": "thirty five point four",
+                   },
+               ])]):
+    return {"item_id": item_id, "item":item}
+
+
+# @app.post("/index-weights")
+# def create_index_weights(weights: dict[int, float]):
+#     return weights
+#
+#
+# @app.post("/image/multiple")
+# def create_images(images: list[Image]):
+#     return images
+#
+#
+# @app.post("/offers")
+# def create_offers(offer: Offer):
+#     return offer
+#
+#
+# @app.put("/items/{item_id}")
+# def read_data(item_id: int, item: Item):
+#     result = {"item_id": item_id, "item_data": item}
+#     return result
 
