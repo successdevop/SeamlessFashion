48ba9df956e3c2222aea6aee1d19c8a8087fe101
successdevop <successraphael28@gmail.com>
2026-07-10 07:19:59 +0100

    tested deeply nested body models in fastapi using pydantic

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import Annotated, Literal
 
 from fastapi import FastAPI, Path
 from fastapi.params import Query, Body
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
@@ -15,11 +15,18 @@ class FilterParams(BaseModel):
     model_config = {"extra":"forbid"}
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
+    tags: set[str] = set()
+    image: list[Image] | None = None
 
 
 class User(BaseModel):
@@ -27,48 +34,30 @@ class User(BaseModel):
     full_name: str | None = None
 
 
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@app.post("/index-weights")
+def create_index_weights(weights: dict[int, float]):
+    return weights
+
+
+@app.post("/image/multiple")
+def create_images(images: list[Image]):
+    return images
+
+
+@app.post("/offers")
+def create_offers(offer: Offer):
+    return offer
+
+
 @app.put("/items/{item_id}")
-def read_data(item_id: int, item: Item, user: User):
-    result = {"item_id": item_id, "item_data": item, "user_data": user}
+def read_data(item_id: int, item: Item):
+    result = {"item_id": item_id, "item_data": item}
     return result
 
-
-@app.put("/item/{item_id}")
-def read_item(item_id: Annotated[int, Path(title="Item ID")], q: str | None = None, item: Item | None = None):
-    result = {"item_id": item_id}
-    if q:
-        result.update({"q":q})
-    if item:
-        result.update({"item": item})
-    return result
-
-
-@app.get("/item")
-def get_data(filter_query: Annotated[FilterParams, Query()]):
-    print(filter_query.order_by)
-    return filter_query
-
-
-@app.get("/items/{item_id}")
-def read_items(*, item_id: Annotated[int, Path(title="The ID of the item to get")],
-               q: Annotated[str|None, Query(alias="item-query")] = None,
-               size: Annotated[float, Query(gt=0, le=10.5)]):
-    result = {"item_id": item_id}
-    if q:
-        result.update({"q":q})
-    if size:
-        result.update({"size":size})
-    return result
-
-
-@app.post("/items/{id}")
-def create(id: Annotated[int, Path()], item: Item, user: User, importance: Annotated[int, Body()]):
-    return {"id": id, "item_data": item, "user_data": user, "importance": importance}
-
-
-@app.put("/update/{item_id}")
-def update_data(item_id: int, item: Item, user: User, importance: Annotated[int, Body()], q: str | None = None):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    if q:
-        results.update({"q": q})
-    return results
