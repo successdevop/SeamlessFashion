6c07f821817dc9dd36d8338f55f631144c7a132b
successdevop <successraphael28@gmail.com>
2026-07-17 11:45:07 +0100

    tested oauth2 password specification

--- app/dependency.py
+++ app/dependency.py
@@ -1,6 +1,8 @@
 from typing import Annotated, Any
 
 from fastapi import FastAPI, Depends, HTTPException, Header, Cookie
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -145,25 +147,106 @@ app = FastAPI()
 #         raise OwnerError(username)
 #     return item
 
-data = {
-    "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
-    "portal-gun": {"description": "Gun to create portals", "owner": "Rick"}
+# data = {
+#     "plumbus": {"description": "Freshly pickled plumbus", "owner": "Morty"},
+#     "portal-gun": {"description": "Gun to create portals", "owner": "Rick"}
+# }
+#
+#
+# class InternalError(Exception):
+#     pass
+#
+# def get_username():
+#     try:
+#         yield "Rick"
+#     except InternalError as e:
+#         raise HTTPException(status_code=400, detail=f"Oops, we didn't raise again, Britney 😱 || {str(e)}")
+#
+# @app.get("/item/{item_id}")
+# async def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
+#     if item_id == "portal-gun":
+#         raise InternalError(f"The portal gun is too dangerous to be owned by {username}")
+#     if item_id != "plumbus":
+#         raise HTTPException(status_code=400, detail="Item not found")
+#     return item_id
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
 }
 
 
-class InternalError(Exception):
-    pass
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
-def get_username():
-    try:
-        yield "Rick"
-    except InternalError as e:
-        raise HTTPException(status_code=400, detail=f"Oops, we didn't raise again, Britney 😱 || {str(e)}")
 
-@app.get("/item/{item_id}")
-async def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
-    if item_id == "portal-gun":
-        raise InternalError(f"The portal gun is too dangerous to be owned by {username}")
-    if item_id != "plumbus":
-        raise HTTPException(status_code=400, detail="Item not found")
-    return item_id
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username not in db:
+        raise HTTPException(status_code=404, detail="User not found")
+    user = db[username]
+    return UserInDB(**user)
+
+def fake_decode_token(token:str):
+    user = get_user(db=fake_users_db, username=token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token=token)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="Not authenticated",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return user
+
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+
+    if hashed_password != user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_items(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
