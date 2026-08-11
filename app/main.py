b90d2c936178a1d2cfa2ab9976b72bc0b9b0eeac
successdevop <successraphael28@gmail.com>
2026-08-11 20:02:26 +0100

    testing phase of our models and schemas

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.database.db_session import engine
+from app.enums.user_enums import VerificationStatusEnum
+from app.schemas.base_or_shared.address import AddressCreate, AddressRead
 
 logger = logging.getLogger(__name__)
 
@@ -41,9 +43,32 @@ def create_app() -> FastAPI:
         lifespan=lifespan
     )
 
-    @my_app.get("/")
-    async def root():
-        return {"message": "Hello World"}
+    fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+    @my_app.get("/model/{model_name}")
+    async def root(model_name: VerificationStatusEnum):
+        if model_name is VerificationStatusEnum.REJECTED:
+            return {"model_name": model_name, "message":"Deep learning FTW!"}
+
+        if model_name.value == "pending_review":
+            return {"model_name": model_name, "message":"LeCNN all the images"}
+
+        return {"model_name": model_name, "message": "Have some residuals"}
+
+    @my_app.get("/users/{user_id}/items/{item_id}")
+    async def read_user_me(user_id:int, item_id: str, q: str | None = None, short: bool = False):
+        item = {"user_id": user_id, "item_id": item_id}
+        if q:
+            item.update({"q":q})
+
+        if not short:
+            item.update({"description":"This is an amazing item that has a long description"})
+
+        return item
+
+    @my_app.post("/item")
+    async def read_items(item: AddressCreate):
+        return item
 
     return my_app
 
